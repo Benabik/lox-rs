fb7750aa898c401fc0bffd03fa5b876743108bc1
Brian Gernhardt <brian@gernhardtsoftware.com>
2024-08-27 11:58:39 -0400

    Switch to clap for argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,26 @@
-use std::env;
+use clap::{Parser, Subcommand};
+
 use std::fs;
-use std::io::{self, Write};
+use std::path::PathBuf;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    Tokenize { filename: PathBuf },
+}
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 3 {
-        writeln!(io::stderr(), "Usage: {} tokenize <filename>", args[0]).unwrap();
-        return;
-    }
-
-    let command = &args[1];
-    let filename = &args[2];
-
-    match command.as_str() {
-        "tokenize" => {
-            // You can use print statements as follows for debugging, they'll be visible when running tests.
-            writeln!(io::stderr(), "Logs from your program will appear here!").unwrap();
-
-            let file_contents = fs::read_to_string(filename).unwrap_or_else(|_| {
-                writeln!(io::stderr(), "Failed to read file {}", filename).unwrap();
+    let args = Args::parse();
+    match args.command {
+        Commands::Tokenize { filename } => {
+            let file_contents = fs::read_to_string(&filename).unwrap_or_else(|_| {
+                eprintln!("Failed to read file {}", filename.display());
                 String::new()
             });
 
@@ -27,10 +29,6 @@ fn main() {
             } else {
                 println!("EOF  null"); // Placeholder, remove this line when implementing the scanner
             }
-        }
-        _ => {
-            writeln!(io::stderr(), "Unknown command: {}", command).unwrap();
-            return;
         }
     }
 }
