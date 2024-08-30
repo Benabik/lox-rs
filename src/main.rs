79a9f1f7ec58ff52c7d47f2e572aa0553406e0fd
Brian Gernhardt <brian@gernhardtsoftware.com>
2024-08-29 23:10:00 -0500

    Refactor main using early exit, runtime error is 70
    Since each subcommand is just adding to the previous, don't duplicate
    the code at each step.
    
    Now that all earlier errors are handled appropriately, add an error
    exit for evaluate.

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,16 @@ struct Args {
     command: Commands,
 }
 
+impl Args {
+    fn filename(&self) -> &PathBuf {
+        match &self.command {
+            Commands::Evaluate { filename } => filename,
+            Commands::Parse { filename } => filename,
+            Commands::Tokenize { filename } => filename,
+        }
+    }
+}
+
 #[derive(Subcommand, Debug)]
 enum Commands {
     Evaluate { filename: PathBuf },
@@ -27,58 +37,55 @@ fn read_file(filename: &PathBuf) -> miette::Result<String> {
 
 fn main() -> miette::Result<()> {
     let args = Args::parse();
-    match args.command {
-        Commands::Evaluate { filename } => {
-            let file_contents = read_file(&filename)?;
-            let mut lexer = imp::Lexer::new(&file_contents);
-            let mut parser = imp::Parser::new(&mut lexer);
 
-            match imp::evaluate(parser.expression()?) {
-                Ok(val) => println!("{val}"),
+    // Read file
+    let filename = args.filename();
+    let file_contents = read_file(filename)?;
+
+    let mut lexer = imp::Lexer::new(&file_contents);
+    if matches!(args.command, Commands::Tokenize { .. }) {
+        let mut error = false;
+        for token in lexer {
+            match token {
+                Ok(token) => println!("{token}"),
                 Err(e) => {
-                    eprintln!("{e:?}");
-                    std::process::exit(65);
+                    error = true;
+                    eprintln!("{e}"); // Simple for test requirements
                 }
             }
-
-            parser.expect_eof()?;
         }
+        println!("EOF  null");
+        std::process::exit(if error { 65 } else { 0 });
+    }
 
-        Commands::Parse { filename } => {
-            let file_contents = read_file(&filename)?;
-            let mut lexer = imp::Lexer::new(&file_contents);
-            let mut parser = imp::Parser::new(&mut lexer);
-
-            match parser.expression() {
-                Ok(expr) => println!("{expr}"),
-                Err(e) => {
-                    eprintln!("{e:?}");
-                    std::process::exit(65);
-                }
-            }
-
-            parser.expect_eof()?;
+    let mut parser = imp::Parser::new(&mut lexer);
+    let expr = match parser.expression() {
+        Ok(expr) => expr,
+        Err(e) => {
+            eprintln!("{e:?}");
+            std::process::exit(65);
         }
+    };
 
-        Commands::Tokenize { filename } => {
-            let file_contents = read_file(&filename)?;
-            let lexer = imp::Lexer::new(&file_contents);
+    if matches!(args.command, Commands::Parse { .. }) {
+        std::process::exit(0);
+    }
 
-            let mut error = false;
-            for token in lexer {
-                match token {
-                    Ok(token) => println!("{token}"),
-                    Err(e) => {
-                        error = true;
-                        eprintln!("{e}"); // Simple for test requirements
-                    }
-                }
-            }
-            println!("EOF  null");
-            if error {
-                std::process::exit(65);
-            };
+    match imp::evaluate(expr) {
+        Ok(val) => println!("{val}"),
+        Err(e) => {
+            eprintln!("{e:?}");
+            std::process::exit(70);
         }
     }
+
+    parser.expect_eof()?;
+
+    /*
+    if matches!(args.command, Commands::Evaluate { .. }) {
+        std::process::exit(0);
+    }
+    */
+
     Ok(())
 }
