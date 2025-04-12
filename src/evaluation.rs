8d8933e7e38f8be2fcc1bb23a666635079807565
Brian Gernhardt <brian@gernhardtsoftware.com>
2025-04-12 01:30:19 -0400

    rustfmt src/evaluation.rs

--- src/evaluation.rs
+++ src/evaluation.rs
@@ -216,101 +216,102 @@ impl Evaluator {
         let to_string =
             |val: Value, origin: &SourceLoc| String::try_from(val).with_source_loc(origin);
 
-        let val =
-            match expr {
-                Expression::Literal { value, .. } => value.into(),
-                Expression::Grouping { expr, .. } => return self.expression(expr),
-                Expression::Unary { op, expr, origin } => {
-                    let value = self.expression(expr)?;
-                    match op {
-                        parser::UnaryOp::Negate => (-to_float(value, origin)?).into(),
-                        parser::UnaryOp::Not => (!bool::from(value)).into(),
-                    }
+        let val = match expr {
+            Expression::Literal { value, .. } => value.into(),
+            Expression::Grouping { expr, .. } => return self.expression(expr),
+            Expression::Unary { op, expr, origin } => {
+                let value = self.expression(expr)?;
+                match op {
+                    parser::UnaryOp::Negate => (-to_float(value, origin)?).into(),
+                    parser::UnaryOp::Not => (!bool::from(value)).into(),
                 }
-                Expression::Assign { name, expr, origin } => {
-                    let value = self.expression(expr)?;
-                    let var = self.lookup(name, origin)?;
-                    *var = value.clone();
-                    value
-                }
-                Expression::Binary {
-                    op,
-                    lhs,
-                    rhs,
-                    origin,
-                } => {
-                    let lhs = self.expression(lhs)?;
+            }
+            Expression::Assign { name, expr, origin } => {
+                let value = self.expression(expr)?;
+                let var = self.lookup(name, origin)?;
+                *var = value.clone();
+                value
+            }
+            Expression::Binary {
+                op,
+                lhs,
+                rhs,
+                origin,
+            } => {
+                let lhs = self.expression(lhs)?;
 
-                    // Evaluate logical ops before RHS for short-circuiting
-                    if matches!(op, Or | And) {
-                        let truth = bool::from(&lhs);
-                        return Ok(match (truth, op) {
-                            (true, Or) => lhs,
-                            (false, And) => lhs,
-                            _ => self.expression(rhs)?,
-                        });
-                    }
-
-                    let rhs = self.expression(rhs)?;
-
-                    let binary_float = |lhs, rhs, f: fn(f64, f64) -> f64| {
-                        let lhs = to_float(lhs, origin)?;
-                        let rhs = to_float(rhs, origin)?;
-                        Ok::<Value, miette::Report>(f(lhs, rhs).into())
-                    };
-
-                    use parser::BinaryOp::*;
-                    match op {
-                        Or | And => unreachable!("matched above"),
-                        Equal => Value::from(lhs == rhs),
-                        NotEqual => Value::from(lhs != rhs),
-                        Less | LessEqual | Greater | GreaterEqual => match lhs {
-                            Value::Number(lhs) => {
-                                let rhs = to_float(rhs, origin)?;
-                                match op {
-                                    Less => lhs < rhs,
-                                    LessEqual => lhs <= rhs,
-                                    Greater => lhs > rhs,
-                                    GreaterEqual => lhs >= rhs,
-                                    _ => unreachable!("by outer match"),
-                                }
-                                .into()
-                            }
-                            Value::String(lhs) => {
-                                let rhs = to_string(rhs, origin)?;
-                                match op {
-                                    Less => lhs < rhs,
-                                    LessEqual => lhs <= rhs,
-                                    Greater => lhs > rhs,
-                                    GreaterEqual => lhs >= rhs,
-                                    _ => unreachable!("by outer match"),
-                                }
-                                .into()
-                            }
-                            _ => {
-                                return Err(TypeError::new("number or string", lhs)
-                                    .with_source_loc(origin))
-                            }
-                        },
-                        Plus => match lhs {
-                            Value::Number(lhs) => (lhs + to_float(rhs, origin)?).into(),
-                            Value::String(mut lhs) => {
-                                lhs += &to_string(rhs, origin)?;
-                                lhs.into()
-                            }
-                            _ => {
-                                return Err(TypeError::new("number or string", lhs)
-                                    .with_source_loc(origin))
-                            }
-                        },
-                        Minus => binary_float(lhs, rhs, |x, y| x - y)?,
-                        Multiply => binary_float(lhs, rhs, |x, y| x * y)?,
-                        Divide => binary_float(lhs, rhs, |x, y| x / y)?,
-                    }
+                // Evaluate logical ops before RHS for short-circuiting
+                if matches!(op, Or | And) {
+                    let truth = bool::from(&lhs);
+                    return Ok(match (truth, op) {
+                        (true, Or) => lhs,
+                        (false, And) => lhs,
+                        _ => self.expression(rhs)?,
+                    });
                 }
 
-                Expression::Variable { name, origin } => self.lookup(name, origin)?.clone(),
-            };
+                let rhs = self.expression(rhs)?;
+
+                let binary_float = |lhs, rhs, f: fn(f64, f64) -> f64| {
+                    let lhs = to_float(lhs, origin)?;
+                    let rhs = to_float(rhs, origin)?;
+                    Ok::<Value, miette::Report>(f(lhs, rhs).into())
+                };
+
+                use parser::BinaryOp::*;
+                match op {
+                    Or | And => unreachable!("matched above"),
+                    Equal => Value::from(lhs == rhs),
+                    NotEqual => Value::from(lhs != rhs),
+                    Less | LessEqual | Greater | GreaterEqual => match lhs {
+                        Value::Number(lhs) => {
+                            let rhs = to_float(rhs, origin)?;
+                            match op {
+                                Less => lhs < rhs,
+                                LessEqual => lhs <= rhs,
+                                Greater => lhs > rhs,
+                                GreaterEqual => lhs >= rhs,
+                                _ => unreachable!("by outer match"),
+                            }
+                            .into()
+                        }
+                        Value::String(lhs) => {
+                            let rhs = to_string(rhs, origin)?;
+                            match op {
+                                Less => lhs < rhs,
+                                LessEqual => lhs <= rhs,
+                                Greater => lhs > rhs,
+                                GreaterEqual => lhs >= rhs,
+                                _ => unreachable!("by outer match"),
+                            }
+                            .into()
+                        }
+                        _ => {
+                            return Err(
+                                TypeError::new("number or string", lhs).with_source_loc(origin)
+                            )
+                        }
+                    },
+                    Plus => match lhs {
+                        Value::Number(lhs) => (lhs + to_float(rhs, origin)?).into(),
+                        Value::String(mut lhs) => {
+                            lhs += &to_string(rhs, origin)?;
+                            lhs.into()
+                        }
+                        _ => {
+                            return Err(
+                                TypeError::new("number or string", lhs).with_source_loc(origin)
+                            )
+                        }
+                    },
+                    Minus => binary_float(lhs, rhs, |x, y| x - y)?,
+                    Multiply => binary_float(lhs, rhs, |x, y| x * y)?,
+                    Divide => binary_float(lhs, rhs, |x, y| x / y)?,
+                }
+            }
+
+            Expression::Variable { name, origin } => self.lookup(name, origin)?.clone(),
+        };
 
         Ok(val)
     }
